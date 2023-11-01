2cc6bafc1b31e9c0202b190e57068111fd03a33b
MyahGrieve <myahkg@gmail.com>
2023-11-01 10:30:27 -0600

    added stuff

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+//import { PageHeader } from './components/header/PageHeader'
+
+//LANDING PAGE, first page that shows up
+export default function Home() {
+  return (
+    <>
+   {/* // <PageHeader/> */}
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* <h1>Home page Root</h1>
+      <p> paths "/"</p> */}
+    </main>
+    </>
+  )
+}
