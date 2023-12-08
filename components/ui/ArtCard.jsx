cffcd2ef737e66d10dd76eaa0b0845bf4d33a3f3
MyahGrieve <myahkg@gmail.com>
2023-12-08 13:22:18 -0700

    deleted some test code

--- components/ui/ArtCard.jsx
+++ components/ui/ArtCard.jsx
@@ -25,6 +25,7 @@ const id = item.id
           <CardFooter className="text-small justify-between">
               <p>{item.title}</p>
               <p className="text-default-500" >{item.price}</p>
+              
           </CardFooter>  
         </Card>
            )
