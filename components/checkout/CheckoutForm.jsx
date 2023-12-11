83026e841e7a531d683f9980f94542e1cb1407eb
MyahGrieve <myahkg@gmail.com>
2023-12-11 12:15:49 -0700

    added more recordst o stripe

--- components/checkout/CheckoutForm.jsx
+++ components/checkout/CheckoutForm.jsx
@@ -1,32 +1,30 @@
 import { Button } from "@nextui-org/react"
 
 
- 
- function CheckoutForm ( {price_id}){
-   
- 
-    const details={
-        id:"from dashboard",
-        quanity:2,
-        price:"from dashboard"
-    }
 
-  console.log(price_id)
+function CheckoutForm({ price_id }) {
+
+
     return (
-        <form action='/api/checkout' method="POST" >
-            <input type="hidden" name="id" value="from dashboard"  />
+        <>
+        {price_id ? (
+            <form action='/api/checkout' method="POST" >
+            <input type="hidden" name="id" value="from dashboard" />
             <input type="hidden" name="price" value={price_id} />
             <Button type="suBmit" id="checkout-button" radius="full" className="tracking-light bg-black text-white w-full">Checkout</Button>
-            {/* <button type="submit" id="checkout-button" radius="full" className="tracking-light bg-black text-white w-full">Add to Cart</button> */}
 
         </form>
+        ) : (
+            <Button  radius="full" className="tracking-light bg-black text-white w-full">Checkout</Button>
+
+        )}
         
-        
+</>
+
     )
-    
- }
+
+}
 
 
 
- export {CheckoutForm}
- 
+export { CheckoutForm }
